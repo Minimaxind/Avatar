882e257939ce6c516d7500af5bb07e514144b8fe
Minimaxind <Maksim4877@inbox.ru>
2026-04-06 22:04:46 +0300

    save

--- Source/Avatar/Avatar.cpp
+++ Source/Avatar/Avatar.cpp
@@ -1,4 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
 #include "Avatar.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, Avatar, "Avatar");
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Avatar, "Avatar" );
